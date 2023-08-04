85080aa87ffe5e06088e4f5049567b54093967b4
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-08-04 17:49:20 +0530

    Using Memoization

---
+++ Max-Sum-Adjacent.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+
+int f(int ind, vector<int> &nums, vector<int> &dp )
+{
+    if(ind == 0) return nums[ind];
+    if(ind < 0) return 0;
+    if(dp[ind] != -1) return dp[ind];
+    int pick = nums[ind] + f(ind-2, nums, dp);
+    int notPick = 0 + f(ind-1, nums, dp);
+
+    return dp[ind] = max(pick, notPick);
+}
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int n = nums.size();
+    vector<int> dp(n, -1);
+    return f(n-1, nums, dp);
+}
