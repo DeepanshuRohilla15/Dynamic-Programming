b2966634adb81bfe9206000d863c551fd8b7299a
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-08-09 12:40:51 +0530

    Tabulation -> Space Optimisation

---
+++ Unique-path_block.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<int> prev(m,0);
+        
+        for(int i=0; i<n; i++)
+        {
+            vector<int> cur(m,0);
+            for(int j=0; j<m; j++)
+            {
+                if(obstacleGrid[i][j] == 1) cur[j] =0;
+                else if(i==0 && j==0) cur[j] = 1;
+                else
+                {
+                    int up=0;
+                    int left=0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = cur[j-1];
+                    cur[j] = up + left;
+                }
+            }
+            prev = cur;
+        }
+        return prev[m-1];
+    }
+};
