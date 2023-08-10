649f0afdf3eefb7c650137b6adeab36927195311
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-08-10 16:32:17 +0530

    Tabulation -> Space Optimisation

---
+++ Minimum_Falling_Path_sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> prev(m,0), cur(m,0);
+        
+        for(int j=0; j<m; j++) prev[j] = matrix[0][j];
+        
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                int up = matrix[i][j] + prev[j];
+                
+                int ld = matrix[i][j];
+                if(j-1>=0) ld += prev[j-1];
+                else ld += 1e9;
+                
+                int rd = matrix[i][j];
+                if(j+1<m) rd += prev[j+1];
+                else rd += 1e9;
+                
+                cur[j] = min(up, min(ld, rd));
+            }
+            prev = cur;
+        }
+        int mini = 1e9;
+        for(int j=0; j<m; j++)
+        {
+            mini = min(mini, prev[j]);
+        }
+        
+        return mini;
+    }
+};
