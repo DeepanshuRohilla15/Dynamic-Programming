60e861222e506edfca141ed4f4f21eace2b02ea1
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-08-11 16:53:57 +0530

    Tabulation -> Space Optimisation

---
+++ Chocolat_Pickup.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+
+int maximumChocolates(int n, int m, vector<vector<int>> &grid) {
+    // Write your code here.
+    vector<vector<int>> front(m, vector<int> (m,0));
+    vector<vector<int>> cur(m, vector<int> (m,0));
+    
+    // base case
+    for(int j1=0; j1 <m; j1++)
+    {
+        for(int j2=0; j2<m; j2++)
+        {
+            if(j1 == j2)
+            {
+                front[j1][j2] = grid[n-1][j1];
+            }
+            else{
+                front[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+    }
+
+    // exlpore all the paths
+    for(int i= n-2; i>=0; i--)
+    {
+        for(int j1=0; j1<m; j1++)
+        {
+            for(int j2=0; j2<m; j2++)
+            {
+                int maxi= 0;
+                for(int dj1 = -1; dj1<=1; dj1++)
+                {
+                    for(int dj2=-1; dj2<=1; dj2++)
+                    {
+                        int value =0;
+                        if(j1 == j2) value = grid[i][j1];
+                        else value = grid[i][j1] + grid[i][j2];
+                        if(j1 + dj1 >=0 && j1 + dj1<m && j2 + dj2>=0 && j2+dj2<m)
+                           value += front[j1+ dj1][j2 + dj2];
+                        else
+                            value += -1e9;
+                        maxi = max(maxi, value);
+                    }
+                    
+                }
+                 cur[j1][j2] =  maxi;     
+            }
+        }
+        front = cur;
+    }
+    return front[0][m-1];
+}
