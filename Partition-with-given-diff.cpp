4c0e1e73106a86a2632cb3696c2e35421cced8bc
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-08-24 16:57:54 +0530

    Tabulation -> Space Optimisation

---
+++ Partition-with-given-diff.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+int mod = (int)(1e9+7);
+int findWays(vector<int>& arr, int k)
+{
+	// Write your code here.
+	
+	int n = arr.size();
+	vector<int> prev(k+1, 0), cur(k+1, 0);
+	if(arr[0] == 0) prev[0] = 2;
+    else prev[0] = 1;
+	if(arr[0] !=0 && arr[0] <= k) prev[arr[0]] = 1;
+
+	for(int ind=1; ind<n; ind++)
+	{
+		for(int sum=0; sum<=k; sum++)
+		{
+			int notPick = prev[sum];
+			int pick=0;
+			if(arr[ind] <= sum) pick = prev[sum-arr[ind]];
+
+			 cur[sum] = (pick + notPick)%mod;
+		}
+        prev = cur;
+	}
+	return prev[k];
+}
+
+int countPartitions(int n, int d, vector<int> &arr) {
+    // Write your code here.
+    int totSum=0;
+    for(auto &it: arr) totSum += it;
+    if(totSum - d <0 || (totSum-d)%2) return false;
+    return findWays(arr, (totSum-d)/2);
+}
