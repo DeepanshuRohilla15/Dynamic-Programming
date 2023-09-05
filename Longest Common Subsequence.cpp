b60ac5e5588ff99d5a8dbe848ca6bf9f540e862a
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-09-05 15:19:08 +0530

    Memoization

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+int f(int i, int j, string&s, string &t, vector<vector<int>> &dp)
+{
+	if(i==0 || j==0 ) return 0;
+	if(dp[i][j] != -1) return dp[i][j];
+	if(s[i-1]==t[j-1]) return dp[i][j] = 1 + f(i-1, j-1, s, t, dp);
+
+	return dp[i][j] = max(f(i-1, j, s, t, dp), f(i, j-1, s, t, dp));
+}
+
+int lcs(string s, string t)
+{
+	int n = s.size();
+	int m = t.size();
+	vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+	return f(n, m, s, t, dp);
+}
