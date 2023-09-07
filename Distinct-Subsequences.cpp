eeb2ff01ecbe6ea500981d1796499b19f0a42f0c
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-09-07 22:34:28 +0530

    Space Optimisation

---
+++ Distinct-Subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mod = (int)(1e9+7);
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<int> prev(m+1, 0), cur(m+1, 0);
+        
+        prev[0] = cur[0] = 1;
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s[i-1] == t[j-1])
+                {
+                    cur[j] = prev[j-1]%mod + prev[j]%mod;
+                }
+                else cur[j] = prev[j]%mod;
+            }
+            prev = cur;
+        }
+        return (prev[m]);
+    }
+};
