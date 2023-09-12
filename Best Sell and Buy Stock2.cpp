961bfd4ad1c4ff4a2debadc49ee1e2ae69222e85
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-09-12 12:26:45 +0530

    Space Optimisation

---
+++ Best Sell and Buy Stock2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+long getMaximumProfit(long *values, int n)
+{
+    vector<long> ahead(2,0), cur(2,0);
+
+    ahead[0] = ahead[1] = 0;
+
+    for(int ind=n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            long profit=0;
+            if(buy){
+                profit = max(-values[ind] + ahead[0],
+                            0 + ahead[1]); 
+            }
+            else{
+                profit = max(values[ind] + ahead[1],
+                            0 + ahead[0]);
+            }
+             cur[buy] = profit;
+        }
+        ahead = cur;
+    }
+
+    return ahead[1];
+}
