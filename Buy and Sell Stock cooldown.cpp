033945cec8a288423b2a1d05079a2b545111fc43
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-09-14 00:20:03 +0530

    Memoization

---
+++ Buy and Sell Stock cooldown.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+int f(int ind, int buy, vector<int> &prices, vector<vector<int>> &dp)
+{
+    if(ind >= prices.size()) return 0;
+
+    if(dp[ind][buy] != -1) return dp[ind][buy];
+    if(buy==1){
+        return dp[ind][buy] = max(-prices[ind] + f(ind+1, 0, prices, dp),
+                  0 + f(ind+1, 1, prices, dp));
+    }
+    return dp[ind][buy] =max(prices[ind] + f(ind+2, 1, prices, dp),
+               f(ind+1, 0, prices,dp));
+}
+
+int stockProfit(vector<int> &prices){
+    // Write your code here.
+    int n = prices.size();
+    vector<vector<int>> dp(n, vector<int>(2, -1));
+    return f(0, 1, prices, dp);
+}
