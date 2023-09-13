00034c2309731d13b50bc3cc9fbdbeb52520b173
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2023-09-14 00:25:48 +0530

    Tabulation

---
+++ Buy and Sell Stock4.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+int stockProfit(vector<int> &prices){
+    // Write your code here.
+    int n = prices.size();
+    vector<vector<int>> dp(n+2, vector<int>(2, 0));
+
+    for(int ind=n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            if(buy==1){
+                dp[ind][buy] = max(-prices[ind] + dp[ind+1][0],
+                        0 + dp[ind+1][1]);
+            }
+            else dp[ind][buy] =max(prices[ind] + dp[ind+2][1],
+                    dp[ind+1][0]);
+        }
+    }
+
+    return dp[0][1];
+}
